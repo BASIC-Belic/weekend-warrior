0061917b6bb667caf4d8f2acf768e21e089d3956
Chantelle Belic <chantelle.belic@gmail.com>
2018-08-21 13:52:02 -0700

    semret and hayden's

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -3,9 +3,14 @@
 # except every year that is evenly divisible by 100
 # unless the year is also evenly divisible by 400
 
+# def leap_year?(year = nil)
+#   if year % 4 == 0 && year % 100 != 0
+#     return true
+#   elsif year % 400 == 0
+#     return true
+#   end
+# end
+
 def leap_year?(year = nil)
-  if year % 4 == 0 && year % 100 != 0
-    return true
-  elsif year % 400 == 0
-    return true
+return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
 end
