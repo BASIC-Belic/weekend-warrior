589b6e5c9e42c93e544bb288ff93411a3773f5e7
Chantelle Belic <chantelle.belic@gmail.com>
2018-08-28 09:09:37 -0700

    with middle man

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -6,15 +6,26 @@ class Robot
 
   def initialize
 
-    @name = (Array('A'..'Z').sample(2) + Array(0..9).sample(3)).join
+    # @name = (Array('A'..'Z').sample(2) + Array(0..9).sample(3)).join
+    middle_man
 
   end
-
 
   def reset
 
-    @name = (Array('A'..'Z').sample(2) + Array(0..9).sample(3)).join
+    # @name = (Array('A'..'Z').sample(2) + Array(0..9).sample(3)).join
+    middle_man
+
 
   end
 
+  private
+
+  def middle_man
+
+      @name = (Array('A'..'Z').sample(2) + Array(0..9).sample(3)).join
+  end
+
 end
+
+#OR ADD middle man that does all the work, and call middle man method in initialize and reset
