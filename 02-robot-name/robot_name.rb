621de918acc7e115bd65f25e1cd5d2a1fbda6315
Chantelle Belic <chantelle.belic@gmail.com>
2018-09-03 11:02:20 -0700

    robot name changes.. other ways to implement

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
+# ALPHA_CHARS = [*"A".."Z"]
+# DIGIT_CHARS = [*0..9]
+
+#2.times do ALPHA_CHARS.sample(1) --> so you can have letters repeat
+#3.times doDIGIT_CHARS.sample(1)
+
+
 class Robot
 
   attr_accessor :name
@@ -15,7 +22,6 @@ class Robot
 
     # @name = (Array('A'..'Z').sample(2) + Array(0..9).sample(3)).join
     middle_man
-
 
   end
 
