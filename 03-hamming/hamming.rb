fe5c75ee4ea542fd7270b3fc1380bc5cf69bf293
Chantelle Belic <chantelle.belic@gmail.com>
2018-09-04 09:47:49 -0700

    changed variable name

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,15 +1,15 @@
 class Hamming
 
-  def self.compute(array1, array2)
-    if array1.length != array2.length
+  def self.compute(string1, string2)
+    if string1.length != string2.length
       raise ArgumentError.new('Two strands must be equi length to compare')
     end
 
     counter = 0
 
-    array1.length.times.with_index do |_, i|
+    string1.length.times.with_index do |_, i|
 
-      if array1[i] != array2[i]
+      if string1[i] != string2[i]
         counter += 1
       end
     end
