e865876cca81ab9f73f77c2f1b39b42c8e02d108
Chantelle Belic <chantelle.belic@gmail.com>
2018-09-24 15:49:30 -0700

    first test passing lines_for_verse

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,10 +1,29 @@
 class Alouette
+
+  LES_PARTIES = ["la tête","le bec!", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+
+
   def self.lines_for_verse(verse_num)
+
+    played_lines = []
+    # i = 0
+    (verse_num + 1).times do |i|
+      played_lines << "Et #{LES_PARTIES[i]}!"
+      i += 1
+    end
+
+
+    return played_lines
   end
 
   def self.verse(verse_num)
   end
 
   def self.sing
+
+    first_line = "Alouette, gentille alouette,\nAlouette, je te plumerai. \n\n"
+    second_line = "Je te plumerai"
+    last_line = "A-a-a-ah\n\n"
   end
+
 end
