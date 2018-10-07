e1d1fe0f38d481e7a442939b7a8701c14838a0f0
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-07 14:40:02 -0700

    completed .of_rna and .of_dna self methods with checks

--- 06-rna-transcription/rna_transcription.rb
+++ 06-rna-transcription/rna_transcription.rb
@@ -4,7 +4,12 @@ class Complement
   def self.of_dna(dna_strand)
     rna = []
 
-      dna_strand.chars.each do |nucleotide|
+    dna_strand.chars.each do |nucleotide|
+
+      if nucleotide != 'G' && nucleotide != 'T' && nucleotide !='C' && nucleotide != 'A'
+        raise ArgumentError.new("Not a valid DNA nucleotide.")
+      end
+
       case nucleotide
       when 'G'
         rna << 'C'
@@ -17,26 +22,81 @@ class Complement
       end
     end
 
-    return rna.to_a
+    return rna.join("")
+  end
+
+  def self.of_rna(rna_strand)
+    dna = []
+
+    rna_strand.chars.each do |nucleotide|
+
+      if nucleotide != 'G' && nucleotide != 'U' && nucleotide !='C' && nucleotide != 'A'
+        raise ArgumentError.new("Not a valid DNA nucleotide.")
+      end
+
+      case nucleotide
+      when 'C'
+        dna << 'G'
+      when 'G'
+        dna << 'C'
+      when 'A'
+        dna << 'T'
+      when 'U'
+        dna << 'A'
+      end
+    end
+
+    return dna.join("")
   end
 end
 
+
+#second way:
 # class Complement
 #
 #   def self.of_dna(dna_strand)
-#     nucleotides = dna_strand.split('')
+#     nucleotides = dna_strand.split("")
 #     nucleotides.map! do |nucleotide|
+#
+#       if nucleotide != 'G' && nucleotide != 'T' && nucleotide !='C' && nucleotide != 'A'
+#         raise ArgumentError.new("Not a valid DNA nucleotide.")
+#       end
+#
 #       case nucleotide
+#
 #       when 'G'
 #         'C'
 #       when 'C'
 #         'G'
 #       when 'T'
-#         'T'
+#         'A'
 #       when 'A'
 #         'U'
 #       end
 #     end
-#     return nucleotides
+#     return nucleotides.join("")
+#   end
+#
+#   def self.of_rna(rna_strand)
+#     nucleotides = rna_strand.split("")
+#     nucleotides.map! do |nucleotide|
+#
+#       if nucleotide != 'G' && nucleotide != 'U' && nucleotide !='C' && nucleotide != 'A'
+#         raise ArgumentError.new("Not a valid RNA nucleotide.")
+#       end
+#
+#       case nucleotide
+#
+#       when 'G'
+#         'C'
+#       when 'C'
+#         'G'
+#       when 'U'
+#         'A'
+#       when 'A'
+#         'T'
+#       end
+#     end
+#     return nucleotides.join("")
 #   end
 # end
